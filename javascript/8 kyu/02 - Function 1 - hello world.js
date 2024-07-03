68e70c36a89252158eb5671220ee71a9c9221878
hievgn <hievgn@gmail.com>
2024-07-03 16:17:24 +0500

    init: add solution "Function 1 - hello world"
    Add solution "Function 1 - hello world".

---
+++ javascript/8 kyu/02 - Function 1 - hello world.js
@@ -0,0 +1,8 @@
+// Function 1 - hello world
+// https://www.codewars.com/kata/function-1-hello-world
+
+// Solution:
+function greet() {
+  const greetMessage = "hello world!";
+  return `${greetMessage}`;
+}
