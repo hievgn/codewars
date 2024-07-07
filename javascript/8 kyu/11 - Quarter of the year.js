e726d1d6d74896b444498df4f3190eb0c1724d13
hievgn <hievgn@gmail.com>
2024-07-07 19:23:18 +0500

    init: add solution "Quarter of the year"
    Add solution "Quarter of the year".

---
+++ javascript/8 kyu/11 - Quarter of the year.js
@@ -0,0 +1,18 @@
+// Quarter of the year
+// https://www.codewars.com/kata/quarter-of-the-year
+
+// Solution:
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1;
+  }
+  if (month <= 6) {
+    return 2;
+  }
+  if (month <= 9) {
+    return 3;
+  }
+  if (month <= 12) {
+    return 4;
+  }
+};
